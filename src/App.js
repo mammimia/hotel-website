96c857bb958b74864e016309ef763c616048eb06
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-03-15 17:28:10 +0300

    Footer header and page components initialized

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import './App.css';
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
+import RoomDetails from './pages/RoomDetails';
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">GH Pages test</h1>;
-}
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/room/:id', element: <RoomDetails /> }
+]);
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
